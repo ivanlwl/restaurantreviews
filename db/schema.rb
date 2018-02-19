1b5195306e8027967ea3409a87aadc71949b8f70
Ivan <ivan_protect@yahoo.com.sg>
2018-02-19 12:27:41 +0800

    Add Restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219034352) do
+ActiveRecord::Schema.define(version: 20180219040737) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "price"
     t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
